47673d8d6d4808efc0075b67b87cb8e1b2d930a9
zswang <wangjihu@sensorsdata.cn>
2017-09-09 22:03:27 +0800

    🔨 Add random upper and hash char

--- src/iplus.ts
+++ src/iplus.ts
@@ -20,31 +20,82 @@
  * @license <%- license %>
  */
 /*</jdists>*/
+
+/**
+ * GUID 生成规则
+ */
+export interface GuidParams {
+  /**
+   * 时间起点
+   */
+  starts?: number
+  /**
+   * 是否加入 hash
+   */
+  hash?: boolean
+  /**
+   * 是否随机大写
+   */
+  upper?: boolean
+}
+
 /**
  * 比较大的概率上，生成唯一 ID
  *
- * @param prefix= 前缀
- * @param suffix= 后缀
+ * @param starts= 起始时间，单位：毫秒，默认：0
  * @return 返回生成的 ID
  *
  * @example newGuid:base
   ```js
-  console.log(/^[a-z0-9]+$/.test(iplus.newGuid()));
-  // > true
+    console.log(/^[a-z0-9]+$/.test(iplus.newGuid()));
+    // > true
   ```
- * @example newGuid:prefix+suffix
+ * @example newGuid:starts, hash
   ```js
-  console.log(/^#[a-z0-9]+%$/.test(iplus.newGuid('#', '%')));
-  // > true
+    console.log(/^[a-z0-9]{4,5}$/i.test(iplus.newGuid({
+      starts: Date.now(),
+      hash: true,
+    })));
+    // > true
+  ```
+ * @example newGuid:upper
+  ```js
+    for (var i = 0; i < 100; i++) {
+      var guid = /^[a-z0-9]{4,5}$/i.test(iplus.newGuid({
+        hash: true,
+        upper: true,
+      }));
+      console.log(guid);
+      if (/[A-Z]/.test(guid)) {
+        console.info(guid)
+        // > true
+        break;
+      }
+    }
   ```
   */
-  let newGuid = (function () {
-    let guid = Math.floor(Math.random() * 36)
-    return function newGuid(prefix: string = '', suffix: string = ''): string {
-      return prefix + Date.now().toString(36) + (guid++ % 36).toString(36) + Math.random().toString(36).slice(2, 4) + suffix
+let newGuid = (function () {
+  let guid = Math.floor(Math.random() * 36)
+  return function newGuid(params: GuidParams): string {
+    params = params || {}
+    params.starts = params.starts || 0
+    let data = (Date.now() - params.starts).toString(36) + (guid++ % 36).toString(36) + (Math.random()).toString(36).slice(-2)
+    if (params.hash) {
+      data += (data.split('').map((char) => {
+        return parseInt(char, 36)
+      }).reduce((previous, current) => {
+        return previous ^ current
+      }) % 36).toString(36)
     }
-  })() /*</function>*/
-
-  export {
-    newGuid
+    if (params.upper) {
+      data = data.split('').map((char) => {
+        return Math.random() > 0.5 ? char.toUpperCase() : char
+      }).join('')
+    }
+    return data
   }
+})() /*</function>*/
+
+export {
+  newGuid
+}
--- test/iplus.js
+++ test/iplus.js
@@ -14,14 +14,33 @@ describe("src/iplus.ts", function () {
 
   it("newGuid:base", function () {
     examplejs_printLines = [];
-  examplejs_print(/^[a-z0-9]+$/.test(iplus.newGuid()));
-  assert.equal(examplejs_printLines.join("\n"), "true"); examplejs_printLines = [];
+    examplejs_print(/^[a-z0-9]+$/.test(iplus.newGuid()));
+    assert.equal(examplejs_printLines.join("\n"), "true"); examplejs_printLines = [];
   });
           
-  it("newGuid:prefix+suffix", function () {
+  it("newGuid:starts, hash", function () {
     examplejs_printLines = [];
-  examplejs_print(/^#[a-z0-9]+%$/.test(iplus.newGuid('#', '%')));
-  assert.equal(examplejs_printLines.join("\n"), "true"); examplejs_printLines = [];
+    examplejs_print(/^[a-z0-9]{4,5}$/i.test(iplus.newGuid({
+      starts: Date.now(),
+      hash: true,
+    })));
+    assert.equal(examplejs_printLines.join("\n"), "true"); examplejs_printLines = [];
+  });
+          
+  it("newGuid:upper", function () {
+    examplejs_printLines = [];
+    for (var i = 0; i < 100; i++) {
+      var guid = /^[a-z0-9]{4,5}$/i.test(iplus.newGuid({
+        hash: true,
+        upper: true,
+      }));
+      examplejs_print(guid);
+      if (/[A-Z]/.test(guid)) {
+        console.info(guid)
+        assert.equal(examplejs_printLines.join("\n"), "true"); examplejs_printLines = [];
+        break;
+      }
+    }
   });
           
 });
